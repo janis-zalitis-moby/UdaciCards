ff777fcacfd1090a87af8a58eb561facd12ab4af
janis-zalitis-moby <janis.zalitis@edu2x.com>
2018-01-05 18:20:24 +0200

    refactor: Fixed eslint and cleaned up api.js

--- utils/api.js
+++ utils/api.js
@@ -9,65 +9,63 @@ const initialDecks = {
     questions: [
       {
         question: 'What is React?',
-        answer: 'A library for managing user interfaces'
+        answer: 'A library for managing user interfaces',
       },
       {
         question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
-      }
-    ]
+        answer: 'The componentDidMount lifecycle event',
+      },
+    ],
   },
   JavaScript: {
     title: 'JavaScript',
     questions: [
       {
         question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
-    ]
-  }
-}
+        answer: 'The combination of a function and the lexical environment within which that function was declared.',
+      },
+    ],
+  },
+};
 
 export function startupDecks() {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY)
     .then(JSON.parse)
-    .then((data) => {
+    .then(data => {
       if (data === null) {
         return AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(initialDecks));
       }
       return data;
-    })
+    });
 }
 
 export function getDecks() {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY)
-    .then(JSON.parse)
+    .then(JSON.parse);
 }
 
 export function getDeck(deck) {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY)
     .then(JSON.parse)
-    .then((decks) => decks[deck] || null)
+    .then(decks => decks[deck] || null);
 }
 
 export function saveDeckTitle(deck, title) {
   return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
-    [deck]: { title }
+    [deck]: { title },
   }));
 }
 
 export function addCardToDeck(deck, card) {
-  console.info('addCardToDeck', deck, card);
   return AsyncStorage.getItem(DECKS_STORAGE_KEY)
     .then(JSON.parse)
-    .then((decks) => decks[deck])
+    .then(decks => decks[deck])
     .then(d => {
-      console.info('found deck', d);
       const questions = d.questions || [];
       return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
         [deck]: {
-          questions: [ ...questions, card ]
-        }
-      }))
+          questions: [...questions, card],
+        },
+      }));
     });
 }
